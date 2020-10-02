f2aa8bf8b1208c731f752e2539447e83be9f1464
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-01 20:27:19 -0400

    basic functionality complete

--- README.md
+++
@@ -1,2 +0,0 @@
-# readme-generator
-A command-line application that will allow for the quick generation of a high-quality README. This will allow a developer to spend more time working on their project and less time creating the README.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,50 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  # ${answers.title}
+
+  ## Description
+
+  ${answers.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#tests)
+
+  ## Installation
+
+  ${answers.installation}
+
+  ## Usage
+
+  ${answers.usage}
+  
+  ## License
+
+  ${answers.license}
+
+  ## Contributing
+
+  Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+  Please make sure to update tests as appropriate.
+
+  ## Tests
+
+  console.log(test);
+    
+  The console will display the word 'test'
+
+  ## Questions
+
+  If you would like to learn more, feel free to check out my GitHub or send me an email anytime.
+
+  * [GitHub](https://github.com/${answers.github})
+  * [Repo](https://github.com/Crowe828/${answers.repo})
+  * [Email Me](mailto:${answers.email}?subject=Nice%20README%20Generator%20Dude)
 
 `;
 }
